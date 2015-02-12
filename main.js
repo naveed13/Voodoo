3d485c260246803c320e67ad6c0d16a57d95520b
Naveed <naveed.bilal@gmail.com>
2015-02-12 13:30:22 -0800

    new changes feb 12

--- main.js
+++ main.js
@@ -14,118 +14,67 @@ function Location(loc, hours, footTraffic, percentEnter, donutsPerPerson) {
 
   this.donutHourly = function() {
 
-//------------------Output1 on html - LOCATION NAME------------//
-      // var table2 = document.getElementById("location");
-      // var rowHead3 = document.createElement("td");
-      // rowHead3.textContent = loc;
-      // table2.appendChild(rowHead3);
 
-    // var table1 = document.getElementsByTagName("table")[0];
+      var tableRow1 = document.getElementById("time");
+      var rowCell1 = document.createElement("td");
+      rowCell1.textContent = loc;
+      tableRow1.appendChild(rowCell1);
 
-    // var tableRowLoc = document.createElement("tr");
-    // var rowHeadLoc = document.createElement("th");
-    // rowHeadLoc.textContent = loc;
-    // tableRowLoc.appendChild(rowHeadLoc);
-    // table1.appendChild(tableRowLoc);
-//------------------Output1 ends on html--------------------//
+      var tableRow1 = document.getElementById("hourly");
+      var rowCell1 = document.createElement("td");
+      rowCell1.textContent = "    ";
+      tableRow1.appendChild(rowCell1);
+
+
 
 
     for (var i=this.hours[0];i<=this.hours[1];i++) {
+
+
       var randomNumber=this.findRandom(this.footTraffic);
       var traffic = percentEnter/100;
       var peopleEnter = Math.round(randomNumber*traffic);
       var donutPerHour = Math.round(this.donutsPerPerson*peopleEnter);
       var time;
 
+
       if (i >12) {
         time = i-12 +":00pm";
       } else {
         time = i + ":00am";
       }
-      //------------------Output2 on html - TIME and DONUTS---------//
-      // var table1 = document.getElementsByTagName("table")[0];
-      // var tableRow = document.createElement("tr");
-      // var rowHead = document.createElement("td");
-      // //rowHead.textContent = time + ": " + donutPerHour + " donuts required.";
-      // rowHead.textContent = time;
-      // tableRow.appendChild(rowHead);
-      // table1.appendChild(tableRow);
+
 
       total+=donutPerHour;
 
-      // var table2 = document.getElementById("location");
-      // var rowHead3 = document.createElement("td");
-      // rowHead3.textContent = loc;
-      // table2.appendChild(rowHead3);
-
-      var table2 = document.getElementById("location");
-      var rowHead3 = document.createElement("td");
-      rowHead3.textContent = loc;
-      table2.appendChild(rowHead3);
-
-      var table1 = document.getElementById("time");
-      var rowHead2 = document.createElement("td");
-      rowHead2.textContent = time;
-      table1.appendChild(rowHead2);
-
-      var table = document.getElementById("hour-donuts");
-      var rowHead = document.createElement("td");
-      rowHead.textContent = donutPerHour;
-      table.appendChild(rowHead);
+      var tableRow2 = document.getElementById("time");
+      var rowCell2 = document.createElement("td");
+      rowCell2.textContent = time;
+      tableRow2.appendChild(rowCell2);
 
 
-      var table3 = document.getElementById("total");
-      var rowHead4 = document.createElement("td");
-      rowHead4.textContent = total;
-      table3.appendChild(rowHead4);
+      var tableRow3 = document.getElementById("hourly");
+      var rowCell3 = document.createElement("td");
+      rowCell3.textContent = donutPerHour;
+      tableRow3.appendChild(rowCell3);
 
-      // var table4 = document.getElementById("break");
-      // var rowHead5 = document.createElement("td");
-      // rowHead5.textContent = "br";
-      // table4.appendChild(rowHead5);
+   }
 
+      var tableRow1 = document.getElementById("time");
+      var rowCell1 = document.createElement("td");
+      rowCell1.textContent = "Total";
+      tableRow1.appendChild(rowCell1);
 
-      // var totals = document.getElementById("total");
-      // var rowHead4 = document.createElement("td");
-      // rowHead4.textContent = total;
-      // totals.appendChild(rowHead4);
-
-      // var totalDonuts = document.getElementById("total");
-      // var rowHeadTotalDonuts = document.createElement("td");
-      // rowHeadTotalDonuts.textContent = total;
-      // totalDonuts.appendChild(rowHeadTotalDonuts);
-
-//------------------Output2 ends on html----------------------//
-
-
-
-
-    }
-      // var table4 = document.getElementById("break");
-      // var rowHead5 = document.createElement("td");
-      // rowHead5.textContent = </br>;
-      // table4.appendChild(rowHead5);
+      var tableRow4 = document.getElementById("hourly");
+      var rowCell4 = document.createElement("td");
+      rowCell4.textContent = total;
+      tableRow4.appendChild(rowCell4);
 
   }
 
   this.donutTotalPerDay = function() {
 
-//------------------Output3 on html TOTAL DONUTS---------------//
 
-      var totals = document.getElementById("total");
-      var rowHead4 = document.createElement("td");
-      rowHead4.textContent = total;
-      totals.appendChild(rowHead4);
-
-//linebreak = document.createElement("br");
-
-    // var table1 = document.getElementsByTagName("table")[0];
-    // var tableRow3 = document.createElement("tr");
-    // var rowCellTotal = document.createElement("td");
-    // rowCellTotal.textContent = "Total Donuts "+ total;
-    // tableRow3.appendChild(rowCellTotal);
-    // table1.appendChild(tableRow3);
-//------------------Output3 on html-------------------------//
   };
 }
 
@@ -139,45 +88,45 @@ function Location(loc, hours, footTraffic, percentEnter, donutsPerPerson) {
 
 
 function runAppDowntown() {
-  var region = new Location("Downtown", [7, 18], [80, 220], 10, 4);
+  var region = new Location("Downtown", [6, 18], [80, 220], 10, 4);
   region.donutHourly();
-  //region.donutTotalPerDay();
+  region.donutTotalPerDay();
   document.getElementById("Downtown").disabled = true;
 }
 
 function runAppCapitolHill() {
-  var region = new Location("Capitol Hill", [7, 18], [5,45], 45, 2);
+  var region = new Location("Capitol Hill", [6, 18], [5,45], 45, 2);
   region.donutHourly();
-  //region.donutTotalPerDay();
+  region.donutTotalPerDay();
   document.getElementById("CapitolHill").disabled = true;
 
 }
 
 function runAppSouthLake() {
-  var region = new Location("South Lake", [7, 18], [80, 250], 5, 6);
+  var region = new Location("South Lake", [6, 18], [80, 250], 5, 6);
   region.donutHourly();
-  //region.donutTotalPerDay();
+  region.donutTotalPerDay();
   document.getElementById("SouthLake").disabled = true;
 }
 
 function runAppWedgewood() {
-  var region = new Location("Wedgewood", [7, 18], [20, 60], 20, 1.5);
+  var region = new Location("Wedgewood", [6, 18], [20, 60], 20, 1.5);
   region.donutHourly();
-  //region.donutTotalPerDay();
+  region.donutTotalPerDay();
   document.getElementById("Wedgewood").disabled = true;
 }
 
 function runAppBallard() {
-  var region = new Location("Ballard", [7, 18], [25, 175], 33, 1);
+  var region = new Location("Ballard", [6, 18], [25, 175], 33, 1);
   region.donutHourly();
-  //region.donutTotalPerDay();
+  region.donutTotalPerDay();
   document.getElementById("Ballard").disabled = true;
 }
 
 
- document.getElementById("Downtown").addEventListener("click", runAppDowntown);
- document.getElementById("CapitolHill").addEventListener("click", runAppCapitolHill);
- document.getElementById("SouthLake").addEventListener("click", runAppSouthLake);
- document.getElementById("Wedgewood").addEventListener("click", runAppWedgewood);
- document.getElementById("Ballard").addEventListener("click", runAppBallard);
+ document.getElementById("Down").addEventListener("click", runAppDowntown);
+ document.getElementById("Capitol").addEventListener("click", runAppCapitolHill);
+ document.getElementById("South").addEventListener("click", runAppSouthLake);
+ document.getElementById("Wedge").addEventListener("click", runAppWedgewood);
+ document.getElementById("Ball").addEventListener("click", runAppBallard);
 
